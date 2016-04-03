4193ba6961c80772372a92dee01654c9907e0361
Dicee <dcourtinot@gmail.com>
2016-04-03 21:20:34 +0100

    First code commit

---
+++ utils.js
@@ -0,0 +1,38 @@
+// parse url parameters
+var params = {};
+location.search.substr(1).split("&").forEach(function(item) { var keyValue = item.split("="); params[keyValue[0]] = keyValue[1]; });
+
+function displayResponse(response) { 
+	console.log("Response: ", response);
+	document.getElementById("node").innerHTML = JSON.stringify(response); 
+}
+
+function callGitApi(endPoint, callback) {
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("GET", "https://api.github.com/" + endPoint, true);
+	xhttp.onreadystatechange = function() {
+		if (xhttp.readyState == 4 && xhttp.status == 200) {
+			callback(JSON.parse(xhttp.responseText));
+		}
+	};
+	xhttp.send();
+}
+
+Date.prototype.plusDays = function(days) {
+	var copy = this.copy();
+	copy.setDate(this.getDate() + days);
+	return copy;
+};
+
+Date.prototype.roundToDay = function() {
+	var copy = this.copy();
+	copy.setUTCHours       (0);
+	copy.setUTCMinutes     (0);
+	copy.setUTCSeconds     (0);
+	copy.setUTCMilliseconds(0);
+	return copy;
+};
+
+Date.prototype.copy = function() {
+	return new Date(this.getTime());
+};
