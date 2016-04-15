9dd6706c730741355f16c5c7af20bfde54769d45
Dicee <dcourtinot@gmail.com>
2016-04-15 03:00:51 +0100

    Added a couple of commits stats

--- spray-based/src/main/scala/com/dici/gitStats/data/DataTypes.scala
+++ spray-based/src/main/scala/com/dici/gitStats/data/DataTypes.scala
@@ -4,10 +4,15 @@ import spray.json.DefaultJsonProtocol
 
 final case class Committer(name: String, email: String, html_url: String, avatar_url: String)
 final case class Commit(committer: Committer, message: String, date: String)
-final case class IngestedCommits(committers: List[Committer], commits: List[Commit])
+final case class IngestedCommits(
+    committers: List[Committer],
+    commits: List[Commit],
+    commitsCount: List[(String, Int)],
+    commitsByCommitterAndDate: Map[String, List[Commit]]
+)
 
 object GitStatsJsonProtocol extends DefaultJsonProtocol {
   implicit val committerFormat       = jsonFormat4(Committer)
   implicit val commitFormat          = jsonFormat3(Commit)
-  implicit val ingestedCommitsFormat = jsonFormat2(IngestedCommits)
+  implicit val ingestedCommitsFormat = jsonFormat4(IngestedCommits)
 }
--- spray-based/src/main/scala/com/dici/gitStats/data/ingestors/CommitsIngestor.scala
+++ spray-based/src/main/scala/com/dici/gitStats/data/ingestors/CommitsIngestor.scala
@@ -1,17 +1,32 @@
 package com.dici.gitStats.data.ingestors
 
+import java.text.SimpleDateFormat
+
+import com.dici.collection.mutable.Counter
 import com.dici.gitStats.data.{IngestedCommits, Commit, Committer}
 import scala.collection.mutable.{ HashMap => MHashMap, ListBuffer }
 
 class CommitsIngestor extends DataIngestor[Commit, IngestedCommits] {
   private val committersDedupedByProfileAndName = MHashMap[(String, String), Committer]()
   private val commits                           = ListBuffer[Commit]()
+  private val commitsCount                      = new Counter[String]()
 
   override def ingest(commit: Commit) = {
     val committer = commit.committer
     committersDedupedByProfileAndName += (committer.html_url, committer.name) -> committer
     commits                           += commit
+    commitsCount                      += committer.name
   }
 
-  override def result = IngestedCommits(committersDedupedByProfileAndName.values.toList, commits.toList)
+  override def result = IngestedCommits(committersDedupedByProfileAndName.values.toList,
+                                        commits.toList,
+                                        commitsCount.toMap.toArray.sortBy(- _._2).toList,
+                                        commits.toList.groupBy(commit => commit.committer.name + "," + CommitsIngestor.roundToDay(commit.date)))
+}
+
+object CommitsIngestor {
+  private val DATE_FORMAT  = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+  private val ROUND_TO_DAY = new SimpleDateFormat("yyyy-MM-dd")
+
+  def roundToDay(dateString: String): String = ROUND_TO_DAY.format(DATE_FORMAT.parse(dateString))
 }
